d802d3eeb3e321632674c572000b4160805b22e9
beratisahin <beratisahin94@gmail.com>
2021-06-04 11:17:59 +0300

    second commit

--- src/components/kurumsal/ekibimiz/Ekibimiz.js
+++ src/components/kurumsal/ekibimiz/Ekibimiz.js
@@ -1,5 +1,5 @@
-import mr from "./st_mr.jpg";
-import mrs from "./st_mrs.jpg";
+import erkek from "./erkekDoktor.png";
+import bayan from "./bayanDoktor.png";
 import './ekibimiz.css';
 import React from 'react';
 import useDocumentTitle from '../../../useDocumentTitle';
@@ -11,20 +11,20 @@ function Ekibimiz() {
         <div style={{marginTop:"10px",marginBottom:"10px",justifyContent:"flex-end !important"}}>
             <h3 style={{textAlign:"center",color:"#C92798"}}><b>Ekibimiz</b></h3>
             <div className="col-lg-3 col-md-6 col-sm-12 col-12 ekiptemp" >
-                <img src={mr} className="d-block img-responsive w-100 ekippho" title="temizlik" alt="temizlik" ></img>
+                <img src={erkek} className="d-block img-responsive w-100 ekippho" title="temizlik" alt="temizlik" ></img>
                 <p class="ekip-text">1- Mehmet Bilgili </p>
            
             </div>
             <div className="col-lg-3 col-md-6 col-sm-12 col-12 ekiptemp" >
-                <img src={mrs} className="d-block img-responsive w-100 ekippho" title="ilaçlama" alt="koltuk-yıkama" ></img>
+                <img src={bayan} className="d-block img-responsive w-100 ekippho" title="ilaçlama" alt="koltuk-yıkama" ></img>
                 <p class="ekip-text">2- Serap Bilgili  </p>
             </div>
             <div className="col-lg-3 col-md-6 col-sm-12 col-12 ekiptemp" >
-                <img src={mr} className="d-block img-responsive w-100 ekippho" title="merdiven" alt="merdiven"></img>
+                <img src={erkek} className="d-block img-responsive w-100 ekippho" title="merdiven" alt="merdiven"></img>
                 <p class="ekip-text">3- Ahmet Bilgili</p>
             </div>
             <div className="col-lg-3 col-md-6 col-sm-12 col-12 ekiptemp" >
-                <img src={mrs} className="d-block img-responsive w-100 ekippho" title="araç-yıkama" alt="araç yıkama"></img>
+                <img src={bayan} className="d-block img-responsive w-100 ekippho" title="araç-yıkama" alt="araç yıkama"></img>
                 <p class="ekip-text" >4- Ayşe Bilgili</p>
             </div>
             
