c641a6fb7ef22193b9116c153a66ad5ffd5aec86
beratisahin <beratisahin94@gmail.com>
2022-05-21 22:52:15 +0300

    v1.8

--- src/components/hizmetler/Urunler.js
+++ src/components/hizmetler/Urunler.js
@@ -1,4 +1,4 @@
-import "./Hizmet.css";
+import "./Urunler.css";
 import React from 'react'
 import useDocumentTitle from "../../useDocumentTitle";
 import Title from "../title/Title";
@@ -17,19 +17,73 @@ export default function Urunler() {
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingOne">
       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Accordion Item #1
+        RobotX Micro Lazer Cihazı
       </button>
     </h2>
-    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+<div id="collapseOne"  class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body" >
+        <div>
+        <img class="accPho" alt="" src="https://www.capitalmedikal.com/upload/robotx-micro.png" style={{width: "200px", height:"366px"}}></img>
+        <h4>MİKROSKOP ASİSTANLI LAZER EPİLASYON CİHAZI</h4>
+        <p class="urunyazi">Epilasyona etki eden faktörlerin başında kılın kalınlığı, derinliği ve rengi gelmektedir. Ayrıca cildin rengi ve kılın bulunduğu bölgeye göre de uygulanması gereken işlem değişkenlik göstermektedir. Örneğin yüz bölgesinde bulunan ince ve açık renkli tüylerde 810 nm Diode dalga boyu etkili olurken bacak bölgesindeki kalın ve koyu renkli tüylerde 755 nm Alexandrite dalga boyu etkilidir.
+
+RobotX Hybrid Alexandrite Laser epilasyon cihazı kıl köklerinin tahrip edilebilmesi, kalıcı olarak yok olması için en etkili ve güvenli iki dalga boyunu (755 nm Alexandrite - 810 nm Diode ) aynı anda ileten teknolojiye sahiptir.
+
+Kıl yapısı, kıl rengi, ten rengine göre iki tür başlık seçimi mevcuttur. İster buz başlıkla (ütüleme) jel sürülerek yapılan uygulamada ister soğuk hava üflemeli başlıkla 35 J/cm2 çıkabilen enerjiyi stabil bir şekilde kıl köküne iletir.</p>
+         <br/>
+         <p class="urunyazi">
+         RobotX yüksek soğutma kapasitesi sayesinde diğer soğuk hava üflemeli cihazlardan iki katı daha fazla soğuk hava üretir. Üretilen bu soğuk havayı kayba uğramadan, en kısa yoldan başlığa ulaştıran özel tasarıma sahiptir. Turbo soğuk hava üfleme özelliği ile cilt -15 dereceye kadar soğutulur. Atış esnasında hissedilen acı minimum seviyededir.
+
+Tek kullanımlık silikon başlık aparatı sayesinde size hijyenik bir lazer epilasyon deneyimi sunar.
+
+2-3 mm boyundaki tüylere doğrudan uygulama yapılabilir. Yapılan atış kıl kökünü hedef alırken aynı zamanda cilt yüzeyindeki tüyleri kusursuz kıl patlatma özelliği ile yok eder ve pürüzsüz bir cilt ile uygulama biter.
+
+Dört mevsim uygulama özelliğine sahip olan RobotX kombin lazer sistemi sayesinde en inatçı kıl tiplerinde bile 4 – 6 seansta kesin ve kalıcı sonuçlar alabilirsiniz.
+         </p>
+
+         <p class="urunyazi">
+         RobotX Hybrid Alexandrite lazer cihazı ile tek atış kıl kökünü tahrip etmek için yeterlidir fakat diğer IPL cihazlarda ise tam etki alabilmek için üst üste birden fazla atış yapmak gereklidir. RobotX Hybrid Alexandrite lazer cihazı 10 Hz çalışma hızında bile her atışında enerjiyi iletir ve kıl kökünü tahrip eder fakat diğer cihazlarda bu mümkün değildir. Çalışma hızı sayesinde lazerinizi dakikalar içinde bitirebilir ve zamandan tasarruf edebilirsiniz. Diğer lazer cihazların da kalıcı sonuçlar almak oldukça zordur. Bunun nedeni ise yeterli enerji seviyesinde olmamalarıdır yeterli enerji seviyesine ulaşsalar dahi ciltte yanıklara neden olurlar. Fakat RobotX Hybrid Alexandrite lazer cihazı minimum acı ve maksimum enerjiyle cilde zarar vermeden kıl kökünü etkisiz hale getirir.
+         </p>
+        
+        
+        </div>  
+        
+        <br/>
+        <div>
+            <img class="accPho" alt="" src="https://www.capitalmedikal.com/upload/images/robotx-baslik-buzlanan-baslik.png" style={{float: "left", width:"200px", height: "200px"}}></img>
+            <h4>SOĞUK HAVA ÜFLEMELİ BAŞLIK</h4>
+            <p class="urunyazi">Dünya’nın En Güçlü Soğuk Hava Üflemeli Epilasyon Başlığı Xenon ışık led aydınlatma sayesinde loop kullanmadan en ince kılları dahi rahatlıkla görebilirsiniz. Güvenli modda, soğuk havanın cildi soğutacak kadar cilt yüzeyinde durduğundan emin olunmasını sağlayan otomatik zamanlayıcı bulunmaktadır. Yeni nesil değiştirilebilir hijyenik silikon aparat atış esnasında ışınların operatörün gözüne yansımasını ve oluşabilecek göz bozukluklarını engeller. Bu cihazın kullanılmasının önemi oldukça fazladır. RobotX cihazı üfleme etkisi ile deriye zarar vermeden yalnızca kıl köküne temas ederek orayı yakmayı hedefler. Derinize hedef alınmayıp, temas edilmediği için herhangi bir acı hissi de duyulmayacaktır. Diğer lazer epilasyon makinelerinden turbo soğuk hava üflemeli cilt soğutma etkisi lazer epilasyon makinesini ayıran bu özelliktir. – 15 dereceye kadar soğuk hava üfler. Ve güvenlik modu ile derinizi yanığa karşı da korur. Eğer herhangi bir sorunda üfleme durursa lazer otomatik olarak devre dışı bırakılarak kapatılır. Bu da turbo soğuk hava üflemeli cilt soğutma etkisi lazer epilasyon cihazını daha fazla tercih edilir hale getirdi. Üstelik bu cihaz yüz epilasyonu için de çok uygundur. Cilde zarar vermediği için uzmanlar tarafından da önerilmektedir. Böylece yüz de yanık izi olmadan tüysüz bir cilde sahip olabilirsiniz.</p>
+        </div>
+        <br/>
+        <div>
+        <img class="accPho" alt="" src="https://www.capitalmedikal.com/upload/kararan-gozluk.png" style={{float: "left", width: "200px", height: "86px"}}></img>
+        <h4>OTOMATİK KARARAN GÖZLÜK</h4>
+        <p class="urunyazi">RobotXUltra Otomatik Kararan Gözlük, atış sırasında otomatik karararak konforlu bir uygulama sağlar
+
+RobotXUltra Otomatik Kararan Gözlük, tüm RobotXUltra cihazlarıyla birlikte ücretsiz olarak verilir. Koruma Özellikleri: Çok yönlü absorpsiyon. Basit tasarım, etkili koruma boyutu Yüksek kaliteli malzeme, güçlü darbe direnci, koruyucu etkisi ile üretilmiştir dikkat çekicidir Ayarlanabilir kol uzunluğu, en iyi aşınma pozisyonunu kolayca bulmanızı sağlayabilir Özellikle lazer koruma tasarımı için, gözlerinize lazer hasarını etkili bir şekilde engelleyebilirsiniz.</p>
+        </div>
+
+        <br/>
+        <div>
+            <img class="accPho" alt="" src="https://www.capitalmedikal.com/upload/cilt-analizi.png" style={{width: "200px", height: "108px", float: "right"}}></img>
+            <h4>CİLT TİPİ ANALİZ CİHAZI</h4>
+            <p class="urunyazi">Dilerseniz RobotXUltra’nın üzerinde takılı olan Hybrid Alexandrtie başlığını ve Hybrid Diode başlığını dilerseniz jelsiz hijyenik silikon ile tek tek patlatma atış modunda dilersniz buzlanan başlık olarak jel ile birlikte hızlı atışlarla ütüleme modunda kullanabilir toplam 4 farklı cihaz sisteminden aynı cihaz üzerinde faydalanabilirsiniz. Cilt tipi analizi, kendi yüzünüzü ve spesifik ihtiyaçlarınızı tanımanızı kolaylaştırır. Yağlı, kuru, karma ve normal temel cilt tipleri olsa da bunların kendi arasında çok farklı alt kategorileri bulunur. Yağlı ve akne eğilimli ciltler, kuru ve çizgilenmelere sahip yüzler bu alt kategorilerden bazılarıdır.</p>
+        </div>
+        <br/>
+        <div>
+            <img class="accPho" alt="" src="https://www.capitalmedikal.com/upload/a92ogv6v-1kvqud3-ah4.png" style={{width: "250px", height: "98px"}}></img>
+            <h4>SİEMENS İLE DAHA GÜÇLÜ, BOSCH VEYA DURACELL İLE KESİNTİSİZ </h4>
+            <p class="urunyazi">RobotXUltra üstün kullanım ömrü sunarken elektrik dalgalanmaları veya kesintileri yüzünden meydana gelen arızalar ortadan kalkar. Elektrik akımı düzensizlikleri ile meydana gelen Anakart ve İşlemci arızaları ortadan kalkar.
+
+Dokunmatik ekranlar donmalara, takılmalara ve arızalara karşı korumalı hale gelir. Kullanım ömürleri 10 yıla kadar uzar.</p>
+        </div>
       </div>
     </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingTwo">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Accordion Item #2
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+      Hydrafacial Cihazı
       </button>
     </h2>
     <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
@@ -40,12 +94,15 @@ export default function Urunler() {
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingThree">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
         Accordion Item #3
       </button>
     </h2>
     <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
       <div class="accordion-body">
+      
+
+
         <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
       </div>
     </div>
